142a9018531285f8891bd4bd8b73b9110c82376f
Brendan Smith <brendan5320@gmail.com>
2021-04-23 17:44:21 -0700

    server challenges running

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -14,4 +14,26 @@ describe('WEB SERVER', () => {
     const response = await request.put('/foo');
     expect(response.status).toBe(404);
   });
+
+  it('should respond with 200 on GET /foods', async () => {
+    const response = await request.get('/foods');
+    expect(response.status).toBe(200);
+  });
+  
+  it('should respond with 201 on POST /foods', async () => {
+    const response = await request.post('/foods').send({name: 'testFoodName', color: "testColor" });
+    console.log(response.body);
+    expect(response.status).toBe(201);
+    expect(response.body.name).toEqual('testFoodName');
+  });
+  
+  // it('should respond with 200 on GET /foods', async () => {
+  //   const response = await request.get('/foods');
+  //   expect(response.status).toBe(200);
+  // });
+
+  // it('should respond with 200 on GET /foods', async () => {
+  //   const response = await request.get('/foods');
+  //   expect(response.status).toBe(200);
+  // });
 });
